b8aeda5b98b1a2b3b6054cbe9addb1557ac1a1de
Thomas Lincoln <thmas5952@gmail.com>
2024-09-03 16:19:46 -0300

    teste

--- Assets/Scripts/EnvironmentManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class EnvironmentManager : MonoBehaviour
-{
-    // esse código é usado para separar cada ambiente com um ID proprio, fizemos isso pois no começo
-    // quando colocamos varios ambientes, quando um deles começava uma nova epoca, ele puxava a 
-    // reinicialização de todos, com um id separado eles conseguem reiniciar individualmente.
-    public int environmentID;
-}
